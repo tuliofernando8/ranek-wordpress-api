09fc4a85beeab295328f39670bac1b7d74da83ff
tuliofernando8 <tulio-sv@hotmail.com>
2021-09-21 22:14:55 -0300

    feats: get/post transactions and delete product

--- functions.php
+++ functions.php
@@ -11,6 +11,10 @@ require_once($template_diretorio . "/endpoints/usuario_put.php");
 
 require_once($template_diretorio . "/endpoints/produto_post.php");
 require_once($template_diretorio . "/endpoints/produto_get.php");
+require_once($template_diretorio . "/endpoints/produto_delete.php");
+
+require_once($template_diretorio . "/endpoints/transacao_post.php");
+require_once($template_diretorio . "/endpoints/transacao_get.php");
 
 function get_produto_id_by_slug($slug) {
   $query = new WP_Query(array(
@@ -24,9 +28,23 @@ function get_produto_id_by_slug($slug) {
   return array_shift($posts);
 }
 
+add_action('rest_pre_serve_request', function() {
+  header('Access-Control-Expose-Headers: X-Total-Count');
+});
+
 function expire_token() {
   return time() + (60 * 60 * 24 *7);
 }
 
 add_action('jwt_auth_expire', 'expire_token');
+
+function my_login_screen() { ?>  
+<style type="text/css">
+#login h1 a { background-image: none;}
+
+#backtoblog { display: none;}
+
+</style>
+  <?php }
+add_action('login_enqueue_scripts', 'my_login_screen');
 ?>
